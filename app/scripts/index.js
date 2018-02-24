0b0dee09a028d9f9177d869c89b25f107ec503a0
Alex Berneau <haldir.alex@gmail.com>
2018-02-24 01:09:20 +0100

    :sparkles: about and animations

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,9 +2,24 @@
 
 new TypeIt('.type-it', {
   breakLines: false,
-  speed: 80,
+  speed: 70,
   nextStringDelay: 2000,
-});
+  lifeLike: false,
+})
 
-const App: string = 'my app'
-console.log(App);
+AOS.init()
+
+const home = document.querySelector('.Home')
+const header = document.querySelector('.Header')
+const headerHeight = header.getBoundingClientRect().height
+const homeHeight = home.getBoundingClientRect().height
+
+window.onscroll = () => {
+  if (window.pageYOffset > homeHeight - headerHeight) {
+    header.classList.remove('fadeOutUp')
+    header.classList.add('visible', 'fadeInDown')
+  }
+  if (window.pageYOffset < homeHeight - headerHeight) {
+    header.classList.add('fadeOutUp')
+  }
+}
